846766ff99f613e7b3c046e12c370229fe2790af
Mohd Sanad <mohdsanadzakirizvi@gmail.com>
2015-03-21 00:09:57 +0530

    added files to directory

---
+++ serialcomm.py
@@ -0,0 +1,6 @@
+import serial#import serial module
+ser=serial.Serial('/dev/tty/ACM0',9600)#connect to port and put baudrate
+while(ser.read()):
+    ser.read()#read input
+
+
---
+++ ssh.py
@@ -0,0 +1,8 @@
+import paramiko#import the library
+
+client=paramiko.SSHClient()#open client SSH
+client.set_missing_host_key(paramiko.AutoAddPolicy())
+client.connect(username='username',password='password',port=port1,hostname='hostname')#connect to the server
+sftp=client.open_sftp()#open an sftp connection
+sftp.listdir()#list the directories
+
