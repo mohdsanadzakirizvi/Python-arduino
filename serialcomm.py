2834e146f3776f89e0fe7bbf7d644c6bdf47a310
sanad <mohdsanadzakirizvi@gmail.com>
2016-01-29 20:52:44 +0530

    applied PEP8

--- serialcomm.py
+++ serialcomm.py
@@ -1,6 +1,4 @@
-import serial#import serial module
-ser=serial.Serial('/dev/tty/ACM0',9600)#connect to port and put baudrate
+import serial  # import serial module
+ser = serial.Serial('/dev/tty/ACM0', 9600)  # connect to port and put baudrate
 while(ser.read()):
-    ser.read()#read input
-
-
+    ser.read()  # read input
--- ssh.py
+++ ssh.py
@@ -1,8 +1,8 @@
-import paramiko#import the library
+import paramiko  # import the library
 
-client=paramiko.SSHClient()#open client SSH
+client = paramiko.SSHClient()  # open client SSH
 client.set_missing_host_key(paramiko.AutoAddPolicy())
-client.connect(username='username',password='password',port=port1,hostname='hostname')#connect to the server
-sftp=client.open_sftp()#open an sftp connection
-sftp.listdir()#list the directories
-
+client.connect(username='username', password='password',
+               port=port1, hostname='hostname')  # connect to the server
+sftp = client.open_sftp()  # open an sftp connection
+sftp.listdir()  # list the directories
